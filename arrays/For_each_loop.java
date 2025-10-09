7994951c559319fc565fa6a6361e179d77e69bb1
sujay-chandekar <sujaychandekar10@gmail.com>
2025-10-09 22:42:41 +0530

    Adding for each lopp code

---
+++ arrays/For_each_loop.java
@@ -0,0 +1,10 @@
+package arrays;
+import java.util.*;
+public class For_each_loop{
+    public static void main(String[] args) {
+        int []arr = {10,20,30,40,50};
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+    }
+}
