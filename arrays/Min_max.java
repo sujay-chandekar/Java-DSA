f4adf5a488d119a17383a70401f2bb73dd088b56
sujay-chandekar <sujaychandekar10@gmail.com>
2025-10-14 23:05:13 +0530

    Adding min max code

---
+++ arrays/Min_max.java
@@ -0,0 +1,19 @@
+public class Min_max {
+    public static void main(String[] args) {
+        int []arr = {18,2,3,5,33,63,79,10};
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximun value is : "+max);
+        int min = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum value is : "+min);
+        }
+}
